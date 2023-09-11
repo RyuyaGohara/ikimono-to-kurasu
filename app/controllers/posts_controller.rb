f8ef3300ced2cbd59c19a4fa9cdbc1be7e62bca6
RyuyaGohara <138365197+RyuyaGohara@users.noreply.github.com>
2023-09-11 08:09:23 +0000

    [Add] ヘッダー作成（仮）

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
 
+  def index
+    @posts = Post.all
+  end
+
   def new
     @post = Post.new
   end
@@ -7,21 +11,33 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
-    @post.save
-    redirect_to posts_path
-  end
-
-  def index
-    @posts = Post.all
+    if @post.save
+      flash[:notice] = "投稿に成功しました"
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
   def show
     @post = Post.find(params[:id])
+    @user = @post.user
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
-  
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:notice] = "投稿を更新しました"
+      redirect_to post_path(@post.id)
+    else
+      render :edit
+    end
+  end
+
   def destroy
     post = Post.find(params[:id])
     post.destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def index
+    @user = User.all
+  end
+
   def show
     @user = User.find(params[:id])
     @posts = @user.posts
@@ -10,7 +14,20 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)
+    if @user.update(user_params)
+      flash[:notice] = "ユーザー情報を更新しました"
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   end
+
+
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction, :email)
+  end
+
 end

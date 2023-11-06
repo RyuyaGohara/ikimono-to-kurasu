89e34f9b1f91e0758e41200992e7d525230643b7
RyuyaGohara <138365197+RyuyaGohara@users.noreply.github.com>
2023-11-06 08:47:18 +0000

    フォロー、フォロワー一覧ページ　タイトル修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -13,13 +13,13 @@ class RelationshipsController < ApplicationController
 
 # フォロー一覧用
   def followings
-    user = User.find(params[:user_id])
-    @users = user.followings.page(params[:page])
+    @user = User.find(params[:user_id])
+    @users = @user.followings.page(params[:page])
   end
 # フォロワー一覧用
   def followers
-    user = User.find(params[:user_id])
-    @users = user.followers.page(params[:page])
+    @user = User.find(params[:user_id])
+    @users = @user.followers.page(params[:page])
   end
 
 end

fc3266aff642cd0c9e724071e89fe71fdf7ecbca
RyuyaGohara <138365197+RyuyaGohara@users.noreply.github.com>
2023-12-25 03:15:54 +0000

    users_controller,user/show　チャットボタン追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,26 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    # ページネーション、新着投稿が上になるように
+  # DM機能
+    @currentUserEntry=Entry.where(user_id: current_user.id)
+    @userEntry=Entry.where(user_id: @user.id)
+    if @user.id == current_user.id
+    else
+      @currentUserEntry.each do |cu|
+        @userEntry.each do |u|
+          if cu.room_id == u.room_id then
+            @isRoom = true
+            @roomId = cu.room_id
+          end
+        end
+      end
+      if @isRoom
+      else
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end
+  # ページネーション、新着投稿が上になるように
     @posts = @user.posts.page(params[:page]).order(created_at: :desc)
   end
 
